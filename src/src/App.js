2c163658ea5dbce1c01ecf841864101b35de83bc
Jonathan Erbe <jonerbe1995@gmail.com>
2020-02-11 15:00:14 -0600

    Add files via upload

---
+++ src/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Filter from './Filter';
+import './App.css';
+
+class App extends Component{
+    render() {
+        return (
+            <div className="App">
+                <Filter/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/src/Filter.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+
+class Filter extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            isLoading: false,
+            error: null,
+            searchText: ''
+        };
+    }
+
+    renderLoading() {
+        return <p>Loading...</p>;
+    }
+
+    renderError() {
+        return (
+            <div>
+                ERROR: {this.state.error.message}
+            </div>
+        );
+    }
+
+    renderPosts() {
+        if(this.state.error) {
+            return this.renderError();
+        }
+        let preData = this.state.data.entries, postData = [];
+        console.log(preData);
+        for(var elem in preData){
+            postData.push([preData[elem].Description, elem, preData[elem].API]);
+        }
+        postData.sort();
+
+        if(this.state.searchText.length > 0) {
+            if(postData.length > 0) {
+                return (
+                    <section>
+                        <h2>Results</h2>
+                        {postData.map(post =>
+                            <div className="listItem" key={post[1]}><p align="left">{post[0] + " (from: " + post[2] + " API)"}</p></div>
+                        )}
+                    </section>
+                );
+            }else{
+                return (
+                    <p>No results found!</p>
+                );
+            }
+        }else{
+            return (
+                <p>Try searching for anything! (<b>matches via <u>substring</u> - i.e. "at" would return "cat" and "atlas"</b>)</p>
+            );
+        }
+    }
+
+    async onChangeText(event) {
+        if(this.state.searchText !== event.target.value){
+            this.setState({searchText: event.target.value, isLoading: true});
+            const url = 'https://api.publicapis.org/entries?title=' + this.state.searchText;
+            const res = await fetch(url);
+            const data = await res.json();
+            this.setState({data: data, isLoading: false});
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <input className="searchBar" type='text' name='searchBar' value={this.state.searchText} onChange={this.onChangeText.bind(this)}/>
+                {this.state.isLoading?
+                    this.renderLoading()
+                    :
+                    this.renderPosts()}
+            </div>
+        );
+    }
+}
+
+export default Filter
